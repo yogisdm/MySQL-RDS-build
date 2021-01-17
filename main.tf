241579fa8bfddd31d676d8f93cf7f0aca0581429
yogisdm <31575108+yogisdm@users.noreply.github.com>
2021-01-17 20:10:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,101 +1,17 @@
-provider "aws" {
-  region = "ap-south-1"
+# Create a database server
+resource "aws_db_instance" "default" {
+  engine         = "mysql"
+  engine_version = "5.7"
+  instance_class = "db.t1.micro"
+  name           = "initial_db"
+  username       = "rootuser"
+  password       = "rootpasswd"
+  endpoint       = "mysql-example.com:3306"
+  parameter_group_name = "default.mysql5.7"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  vpc_security_group_ids = [sg-0d3c94d96b36416e7]
+  # etc, etc; see aws_db_instance docs for more
 }
 
-##############################################################
-# Data sources to get VPC, subnets and security group details
-##############################################################
-#data "aws_vpc" "default" {
-#  default = vpc-0d344d8f40eb95322
-#}
 
-#data "aws_subnet_ids" "all" {
-#  vpc_id = vpc-0d344d8f40eb95322
-#}
-
-#data "aws_security_group" "default" {
-#  vpc_id = data.aws_vpc.default.id
-#  name   = "default"
-#}
-
-#####
-# DB
-#####
-module "db" {
-  source = "../../"
-
-  identifier = "demodb"
-
-  # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
-  engine            = "mysql"
-  engine_version    = "5.7.19"
-  instance_class    = "db.t3.small"
-  allocated_storage = 5
-  storage_encrypted = false
-
-  # kms_key_id        = "arm:aws:kms:<region>:<account id>:key/<kms key id>"
-  name     = "mediawikidb"
-  username = "mediawiki"
-  password = "YourPwdShouldBeLongAndSecure!"
-  port     = "3306"
-
-  vpc_security_group_ids = sg-0d3c94d96b36416e7
-
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  multi_az = true
-
-  # disable backups to create DB faster
-  backup_retention_period = 0
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
-
-  enabled_cloudwatch_logs_exports = ["audit", "general"]
-
-  # DB subnet group
-  subnet_ids = subnet-0698441978a443411
-
-  # DB parameter group
-  family = "mysql5.7"
-
-  # DB option group
-  major_engine_version = "5.7"
-
-  # Snapshot name upon DB deletion
-  final_snapshot_identifier = "demodb"
-
-  # Database Deletion Protection
-  deletion_protection = false
-
-  parameters = [
-    {
-      name  = "character_set_client"
-      value = "utf8"
-    },
-    {
-      name  = "character_set_server"
-      value = "utf8"
-    }
-  ]
-
-  options = [
-    {
-      option_name = "MARIADB_AUDIT_PLUGIN"
-
-      option_settings = [
-        {
-          name  = "SERVER_AUDIT_EVENTS"
-          value = "CONNECT"
-        },
-        {
-          name  = "SERVER_AUDIT_FILE_ROTATIONS"
-          value = "37"
-        },
-      ]
-    },
-  ]
-}
