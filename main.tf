154757c5e532ad923c8ad53f352fecc7702cfbc1
yogisdm <31575108+yogisdm@users.noreply.github.com>
2021-01-17 21:48:11 +0530

    code update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_db_subnet_group" "dbmediawiki" {
-  name       = "mediawiki-subnet-group"
+  name       = "Public Subnet"
   subnet_ids = "subnet-0698441978a443411"
 }
 
@@ -19,7 +19,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
   vpc_security_group_ids = ["sg-0d3c94d96b36416e7"]
-  db_instance_group_name = aws_db_subnet_group.dbmediawiki.id
+  db_subnet_group_name = "${aws_db_subnet_group.dbmediawiki.name}"
 }
 
 
